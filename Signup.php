a4e1e50d681495cbe63982302ea34e03d2c4d4f9
Varun Chopra <vc1202@nyu.edu>
2017-05-05 20:35:42 -0400

    "Corrected Ajax Query"

--- Signup.php
+++ Signup.php
@@ -1,46 +1,44 @@
-//Php script to process user's Signup Request
-
 <?php
 session_start();
 error_reporting(0);
 $servername = "localhost";
-$username = "root";
+$usernam = "root";
 $password = "";
-$dbname = "test";
+$dbname = "project";
+$httpStatusCode = 400;
+$httpStatusMsg  = 'Username Already taken';
+$protocol=isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0';
 
-
-
-$connection = new mysqli($servername,$username,$password,$dbname);
+$connection = new mysqli($servername,$usernam,$password,$dbname);
 //Check if the connection is established
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 } 
 
-$username = mysql_real_escape_string($_POST['username']);  //Escape any Special Characters for Usage in Query(Security)
-
-$result1 = $connection->query("SELECT cname FROM customer WHERE cname = '$username'");
+$username1 = mysql_real_escape_string($_POST['username']);//Escape any Special Characters for Usage in Query(Security)
+$result1 = $connection->query("SELECT * FROM $dbname.User WHERE user_id = '$username1'");
 if($result1->num_rows > 0) {
   //Error Message to be sent to Ajax Query Response.
     //die();
-    array('type' => 'error', 'message' => 'Username Already Exists Please Try with Different Username');
-        header('HTTP/1.1 400 Bad Request');
-        header('Content-Type: application/json; charset=UTF-8');
-        echo json_encode($data);
-    
-    
-    $conn->close();
-
-    
-    }
+     header($protocol.' '.$httpStatusCode.' '.$httpStatusMsg);
+     return false;
+    $connection->close();
+}
 else
 
-{   $pass=mysql_real_escape_string($_POST['password']);
+{
+    
+    $pass=mysql_real_escape_string($_POST['password']);
     $password = password_hash($pass,PASSWORD_DEFAULT);
     $firstName=mysql_real_escape_string($_POST['FirstName']);
     $LastName=mysql_real_escape_string($_POST['LastName']);
+    
+    
     $sql = "INSERT INTO Users(Username, FirstName, LastName,Password)
     VALUES ($usermame,$firstName,$LastName ,$password)";
     if ($connection->query($sql) === TRUE) {
+        
+        
     
 } 
 
